7f00e24a52a4cd1dd0451d0c86427428d08e15ea
Jonas Elkayam <jonaselkayam@gmail.com>
2025-12-17 15:49:53 +0200

    11-create-alien -added base alien component

---
+++ code/frontend/spaceinvaders/src/components/Alien.jsx
@@ -0,0 +1,10 @@
+import alienImg from '../assets/space squid.png';
+
+function Alien() {
+    return (
+        <div className="alien">
+            <img src={alienImg} alt="alien" />
+        </div>
+    );
+}
+export default Alien;
