96d31dec7168a553f9bedaa00c68166227e440b7
Jonas Elkayam <jonaselkayam@gmail.com>
2025-12-18 21:32:19 +0200

    12-add-alien-movement -added position{x, y} var in Alien.jsx to help with the alien position -split the board start btn ane game pieces -added alien to board

--- code/frontend/spaceinvaders/src/components/Alien.jsx
+++ code/frontend/spaceinvaders/src/components/Alien.jsx
@@ -1,8 +1,18 @@
 import alienImg from '../assets/space_squid.png';
+import { useState } from 'react';
+import './Alien.css';
 
 function Alien() {
+    //may change it to two different variables x and y
+    const [position, setPosition] = useState({ x: 50, Y: 90 });
+
+    const dynamicStyle = {
+        '--alien-position-x': '${position.x}%',
+        '--alien-position-y': '${position.y}%',
+    };
+
     return (
-        <div className="alien">
+        <div className="alien-container" style={{ dynamicStyle }}>
             <img src={alienImg} alt="alien" />
         </div>
     );
--- code/frontend/spaceinvaders/src/components/Board.jsx
+++ code/frontend/spaceinvaders/src/components/Board.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 import './Board.css';
 import Spaceship from '../components/Spaceship.jsx';
+import Alien from '../components/Alien.jsx';
 
 function Board() {
     const [started, setStarted] = useState(false);
     const handleStart = () => setStarted(true);
 
+    const gamePieces = () => {
+        return (
+            <>
+                <Alien />
+                <Spaceship />
+            </>
+        );
+    };
+
+    const startButton = () => {
+        return (
+            <button className="start-button" onClick={handleStart}>
+                start
+            </button>
+        );
+    };
+
     return (
         <div className="board">
-            {!started && (
-                <button className="start-button" onClick={handleStart}>
-                    start
-                </button>
-            )}
-            {started && <Spaceship />}
+            {!started ? startButton() : gamePieces()}
         </div>
     );
 }
